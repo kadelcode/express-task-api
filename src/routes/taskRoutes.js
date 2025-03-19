82dc9ba2b6d44fe4fca4b61b86d48ea69e6c42f1
kadelcode <kadelcode@gmail.com>
2025-03-19 17:33:36 +0100

    Delete task route

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -6,7 +6,7 @@
 import express from "express";
 
 // Controllers typically contain the business logic for handling requests
-import { createTask, getTasks, updateTask } from "../controllers/taskController.js";
+import { createTask, getTasks, updateTask, deleteTask } from "../controllers/taskController.js";
 
 // Middleware functions are used to perform actions before or after a route handle
 // is executed, such as authentication or authorization.
@@ -21,5 +21,10 @@ const router = express.Router();
 router.post("/", authMiddleware, createTask); // Create a task (protected)
 router.get("/", authMiddleware, getTasks); // Get all tasks (protected)
 router.put("/:id", authMiddleware, updateTask); // Update a task (protected)
+
+// Define a DELETE route for deleting a task.
+// The route is protected by the `authMiddleware` middleware, ensuring only authenticated users can access it.
+// The `deleteTask` controller function is called to handle the delete logic.
+router.delete("/:id", authMiddleware, deleteTask)
 
 export default router;
